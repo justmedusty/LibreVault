09fd376f9a56c75d5dcd856887f65789091e47b8
dustyn <dustyn_gibb@hotmail.com>
2026-06-14 14:37:32 -0400

    initial commit

--- src/filesystem/filesystem_handling.h
+++ src/filesystem/filesystem_handling.h
@@ -7,10 +7,14 @@
 #define LIBREVAULT_FILESYSTEM_HANDLING_H
 #include <string>
 
+#define DEFAULT_CONFIG_FILE_PATH /etc/librevault.conf
+#define DEFAULT_VAULT_FILE_LOCATION /var/lib/librevault/lv.db
+
 enum class FileObfuscation {
     NO_OBFUSCATION,
-
-
+    FILE_CONTENTS_SCRAMBLING, //Make it less obvious that its a KV map and possibly of value
+    EMBEDDED,//Embed the KV map into another file like a dummy binary and hiding it in the heap mapping of the ELF executable etc
+    FILE_LOCATION_RANDOMIZATION,//Everything normal but just put the file in an odd place
 };
 struct FilesystemHandling {
     FilesystemHandling(
